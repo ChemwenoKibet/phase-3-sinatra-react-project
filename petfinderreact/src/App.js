5e67b5e3304511e78da38be90d26d91ec47dda75
Ian <chemwenokibet11@gmail.com>
2023-03-01 21:39:55 +0300

    created the react file

---
+++ petfinderreact/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import LoginForm from './components/Login';
+import SignupForm from './components/Signup';
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ petfinderreact/src/components/Signup.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const SignupForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Send signup data to server for authentication
+    fetch('/signup', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Signup</h2>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" value={username} onChange={handleUsernameChange} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+      </div>
+      <button type="submit">Signup</button>
+    </form>
+
+  );
+};
+
+export default SignupForm
