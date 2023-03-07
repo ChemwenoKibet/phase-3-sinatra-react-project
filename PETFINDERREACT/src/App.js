072f74ea0d4aa94599daf9932913a840f685d1b3
Ian <chemwenokibet11@gmail.com>
2023-03-07 11:52:33 +0300

    Added search functionality

--- PETFINDERREACT/src/App.js
+++ PETFINDERREACT/src/App.js
@@ -1,21 +1,45 @@
+// import './App.css';
+// import DisplayPets from './components/DisplayPets';
+// import LoginForm from './components/Login';
+// import SignupForm from './components/Signup';
+// import './index.css'
+// import { BrowserRouter, Routes, Route} from 'react-router-dom' 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//       <Routes>
+//         <Route path="/Login" element={<LoginForm />} />
+//         <Route path="/Signup" element={<SignupForm />} />
+//         <Route path="/pets" element={<DisplayPets />} />
+//       </Routes>
+//       </BrowserRouter>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
 import DisplayPets from './components/DisplayPets';
 import LoginForm from './components/Login';
 import SignupForm from './components/Signup';
 import './index.css'
-import { BrowserRouter, Routes, Route} from 'react-router-dom' 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/Login" element={<LoginForm />} />
-        <Route path="/Signup" element={<SignupForm />} />
-        <Route path="/pets" element={<DisplayPets />} />
-      </Routes>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/pets" element={<DisplayPets />} />
+          <Route path="/*" element={<Navigate to="/login" />} />
+        </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
